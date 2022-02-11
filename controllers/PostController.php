8cfcd301a5b4b7b6b1bb8a3567a142d3e4d4d88f
Limfin <victor010389@yandex.ru>
2022-02-11 09:55:56 +0400

    Получение данных из базы с сортировкой: SORT_ASK и SORT_DESK. Вывод данных в виде массива. Фильтрация выводимых данных. Вывод только одной записи. Использование метода count() для подсчета записей. Методы findOne() и findAll(). Создание своего sql запроса.

--- controllers/PostController.php
+++ controllers/PostController.php
@@ -79,8 +79,55 @@ class PostController extends AppController
 		//добавление мета тега description для страницы show
 		$this->view->registerMetaTag(['name' => 'descripton', 'content' => 'descripton заданный в PostController.php']);
 
+		//выводит все записи
+		// $cats = Category::find()->all();
 
-		$cats = Category::find()->all();
+		//сортировка выводимых данных из базы. SORT_ASC прямой порядок 1,2,3...10 
+		// $cats = Category::find()->orderBy(['id' => SORT_ASC])->all();
+
+		//сортировка выводимых данных из базы.  SORT_DESK обратный порядок 10,9,8...1
+		// $cats = Category::find()->orderBy(['id' => SORT_DESC])->all();
+
+		//вывод данных в виде массива
+		// $cats = Category::find()->asArray()->all();
+
+		//фильтрация выводимых данных. Есть несколько способов записи:
+		// 1 - where('id=3'):
+		// $cats = Category::find()->asArray()->where('id=3')->all();
+
+		// 2- where(['id' => 4]):
+		// $cats = Category::find()->asArray()->where(['id' => 4])->all();
+
+		// 3- where(['like', 'login', 'am']): выберет все данные, согласно оператору в данном примере "like", где в поле "login" содержится слово "am"
+		// $cats = Category::find()->asArray()->where(['like', 'login', 'am'])->all();
+
+		// 3- where(['<=', 'id', '3']): выберет все данные, согласно оператору в данном примере "<=", где в поле "id" меньше или равно "3"
+		// $cats = Category::find()->asArray()->where(['<=', 'id', '3'])->all();
+
+		//вывод не всех записей, а только одну
+		//первый способ:
+		// $cats = Category::find()->asArray()->where(['<=', 'id', '3'])->limit(1)->all();
+		//второй способ:
+		// $cats = Category::find()->asArray()->where(['<=', 'id', '3'])->one();
+
+		//подсчет количества записей
+		// $cats = Category::find()->asArray()->where(['<=', 'id', '3'])->count();
+
+		//использование метода findOne()
+		// $cats = Category::findOne(['id' => 4]);
+
+		//использование метода findAll()
+		// $cats = Category::findAll(['id' => 4]);
+
+		//создание своего sql запроса
+		//в этом варианте может быть использована sql инъекция:
+		// $query = "SELECT * FROM users WHERE login LIKE '%am%'";
+		// $cats = Category::findBySql($query)->all();
+
+		//лучше использовать такой вариант запроса:
+		$query = "SELECT * FROM users WHERE login LIKE :search";
+		$cats = Category::findBySql($query, [':search' => '%am%'])->all();
+
 
 
 
